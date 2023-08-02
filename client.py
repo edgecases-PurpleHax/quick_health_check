0eda24f868b0c5179eff5a02df466f6c677ec014
rwils83 <rwils83@gmail.com>
2023-08-02 14:26:51 -0400

    Might as well make shit more usable

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import requests
 import subprocess
+ip = "change me to server ip"
 status = subprocess.call(['systemctl', 'is-active', '--quiet', 'ssh'])
 if (status == 0):
     active = "active"
 else:
     active = "inactive"
 
-r = requests.get(url=f"http://192.168.0.132:9000/health_check?active={active}")
+r = requests.get(url=f"http://{ip}:9000/health_check?active={active}")
 print(r.content)
